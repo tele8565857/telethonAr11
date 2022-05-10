e948db75a70b5cc713d406165cb64289f7a9bf92
telethonArab <104981873+telethonArab@users.noreply.github.com>
2022-05-10 05:54:20 +0300

    @telethonArab
    @telethonArab

--- SQL/extras/Source.py
+++ SQL/extras/Source.py
@@ -4,8 +4,8 @@ from datetime import datetime
 from userbot import StartTime, iqthon
 from userbot.Config import Config
 from userbot.plugins import mention
-help1 = ("**☭⦙ كيفيه التنصيب :**")
-help2 = ("**☭⦙ قـائمـه الاوامـر :**\n**☭⦙ قنـاه السـورس :** @iqthon\n**☭⦙ شـرح اوامـر السـورس : @l3ll3**\n**☭⦙ شـرح فـارات السـورس : @teamtelethon** ")
+help1 = ("**♛ ⦙ كيفيه التنصيب :**")
+help2 = ("**♛ ⦙ قـائمـه الاوامـر :**\n**♛ ⦙ قنـاه السـورس :** @iqthon\n**♛ ⦙ شـرح اوامـر السـورس : @l3ll3**\n**♛ ⦙ شـرح فـارات السـورس : @teamtelethon** \n - اوامر الاونلاين تشتغل فقط في المجموعات ")
 TG_BOT = Config.TG_BOT_USERNAME
 TM = time.strftime("%I:%M")
-Sour = f"**.𓄌 : version 7.5  𓇡.** \n.𓄌 : me  {mention}  𓇡. \n**.𓄌 : time  {TM}  𓇡.**\n**.𓄌 : My Bot {TG_BOT} 𓇡.**\n**.𓄌 : Source : @iqthon  𓇡.**"
+Sour = f"**♛ : version 7.6  𓇡.** \n♛ : me  {mention}  𓇡. \n**♛ : time  {TM}  𓇡.**\n**♛ : My Bot {TG_BOT} 𓇡.**\n**♛ : Source : @iqthon  𓇡.**"
