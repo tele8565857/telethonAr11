a3d03dda337de9315bdae80f7c6f5dd5c1bc1780
tele8565857 <104981873+tele8565857@users.noreply.github.com>
2022-06-02 09:18:04 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -10,6 +10,7 @@ class Development(Config):
     TG_BOT_TOKEN = "ضع توكن بوتك"
     TG_BOT_USERNAME = "ضع معرف بوتك"
     PRIVATE_GROUP_BOT_API_ID = -100
+    PM_LOGGER_GROUP_ID = -100
     COMMAND_HAND_LER = "."
     SUDO_COMMAND_HAND_LER = "."
     TZ = "Asia/Baghdad"
