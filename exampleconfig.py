09f62faa5acc37d94e047bd98a5d64e01724baa0
tele8565857 <104981873+tele8565857@users.noreply.github.com>
2022-06-06 13:01:57 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -12,5 +12,4 @@ class Development(Config):
     SUDO_COMMAND_HAND_LER = "."
     TZ = "Asia/Baghdad"
 
-# تليثون العرب - @IQTHON
-# ملاحضه تمسح الكلام العربي وتضع ببدالة الكود 
+# تليثون العرب - @IQTHON 
