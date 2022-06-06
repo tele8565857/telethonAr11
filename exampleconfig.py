578cf0f0c1718e32ce37169ae2d14881525e3ad5
tele8565857 <104981873+tele8565857@users.noreply.github.com>
2022-06-06 13:01:39 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -1,16 +1,13 @@
 from sample_config import Config
-
-
 class Development(Config):
-    APP_ID = 6
-    API_HASH = "الايبي هاش"
-    ALIVE_NAME = "ضع اسمك"
-    DB_URI = "ضع رابط التخزين"
-    STRING_SESSION = "ضع كود تريمكس"
-    TG_BOT_TOKEN = "ضع توكن بوتك"
-    TG_BOT_USERNAME = "ضع معرف بوتك"
+    APP_ID = 
+    API_HASH = ""
+    ALIVE_NAME = ""
+    STRING_SESSION = ""
+    TG_BOT_TOKEN = ""
+    TG_BOT_USERNAME = ""
+    DB_URI = ""
     PRIVATE_GROUP_BOT_API_ID = -100
-    PM_LOGGER_GROUP_ID = -100
     COMMAND_HAND_LER = "."
     SUDO_COMMAND_HAND_LER = "."
     TZ = "Asia/Baghdad"
