5944af160a6a6d52e2ffba5690fa2854d5db8f6e
mido72 <108865554+mido72@users.noreply.github.com>
2022-08-28 14:47:44 +0200

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,15 @@
+from sample_config import Config
+class Development(Config):
+    APP_ID = 
+    API_HASH = ""
+    ALIVE_NAME = ""
+    STRING_SESSION = ""
+    TG_BOT_TOKEN = ""
+    TG_BOT_USERNAME = ""
+    DB_URI = ""
+    PRIVATE_GROUP_BOT_API_ID = -100
+    COMMAND_HAND_LER = "."
+    SUDO_COMMAND_HAND_LER = "."
+    TZ = "Asia/Baghdad"
+
+# تليثون العرب - @IQTHON 
